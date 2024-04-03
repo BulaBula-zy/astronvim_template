03e04d2a34b2943b2629b39461af5c93cc619e70
BulaBula_zy <1739634190@qq.com>
2024-04-03 23:57:04 +0800

    feat(markdown-preview): add plugin markdown-preview

---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,20 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  event = "User AstroFile",
+  opts = {},
+  ft = { "markdown" },
+  -- if markdown-preview is not working well, plz see https://github.com/iamcco/markdown-preview.nvim/issues/558 for details
+  build = "cd app && npm install",
+  config = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+    vim.g.mkdp_auto_close = true
+    vim.g.mkdp_open_to_the_world = false
+    vim.g.mkdp_open_ip = "127.0.0.1"
+    vim.g.mkdp_port = "8787"
+    vim.g.mkdp_browser = ""
+    vim.g.mkdp_echo_preview_url = true
+    vim.g.mkdp_page_title = "${name}"
+  end,
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  keys = { { "<leader>um", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" } },
+}
