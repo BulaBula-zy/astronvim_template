2998b19c9488bd6884580efec476488fe5e95d1f
BulaBula_zy <1739634190@qq.com>
2024-12-13 14:56:02 +0800

    fix(vim-move): change key mapping to <C-?>
    * solve conflicts between aerospace and vim-move

--- lua/plugins/vim-move.lua
+++ lua/plugins/vim-move.lua
@@ -1,5 +1,18 @@
--- NOTE: use `<OPTION-h/j/k/l>` to move current character or selection
+return {
+  "matze/vim-move",
+  event = { "User AstroFile", "InsertEnter" },
+  config = function()
+    vim.g.move_key_modifier = "C"
+    vim.g.move_key_modifier_visualmode = "C"
 
--- FIX: it seems that using `<OPTION-h>` to move character or selection can not work
+    vim.keymap.set("n", "<C-k>", "<Plug>MoveLineUp", { desc = "Move line up" })
+    vim.keymap.set("n", "<C-j>", "<Plug>MoveLineDown", { desc = "Move line down" })
+    vim.keymap.set("n", "<C-h>", "<Plug>MoveCharLeft", { desc = "Move character left" })
+    vim.keymap.set("n", "<C-l>", "<Plug>MoveCharRight", { desc = "Move character right" })
 
-return {}
+    vim.keymap.set("x", "<C-k>", "<Plug>MoveBlockUp", { desc = "Move block up" })
+    vim.keymap.set("x", "<C-j>", "<Plug>MoveBlockDown", { desc = "Move block down" })
+    vim.keymap.set("x", "<C-h>", "<Plug>MoveBlockLeft", { desc = "Move block left" })
+    vim.keymap.set("x", "<C-l>", "<Plug>MoveBlockRight", { desc = "Move block right" })
+  end,
+}
